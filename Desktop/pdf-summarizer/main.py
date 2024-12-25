a9caa3d53fc9eb7f9fcd2fea460b07612587dfd5
dharsann <131436118+dharsann@users.noreply.github.com>
2024-12-25 14:48:03 +0530

    Update main.py

--- Desktop/pdf-summarizer/main.py
+++ Desktop/pdf-summarizer/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI,File,UploadFile,Form
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
-import fitz,uvicorn
+import fitz
+
 app=FastAPI()
-# allow CORS for UI testing
+
 app.add_middleware(CORSMiddleware,allow_origins=["*"],allow_credentials=True,allow_methods=['*'],allow_headers=["*"],)
-# variable to store extracted text
+
 uploaded_text={}
+
 @app.post("/upload/")
 async def upload_file(file:UploadFile = File(...)):
     global uploaded_text
@@ -17,12 +19,11 @@ async def upload_file(file:UploadFile = File(...)):
                 content+=page.get_text()
     else:
         content=(await file.read()).decode('utf-8')
-        # Store the extracted text in a variable
     uploaded_text[file.filename] = content
     return {"filename": file.filename, "content": content}   
+
 @app.get("/get-text/")
 async def get_text(filename: str = Form(...)):
-    # Return text for a given filename
     return {"filename": filename, "content": uploaded_text.get(filename, "File not found.")}
 
 @app.get("/")
